cb616487cd0768efbfd9680dad167aa5a34abeae
Wellington Prado <wellingtonprado@iMac-Pro-de-Wellington.local>
2021-10-23 02:00:32 -0300

    Modulo 2: Gofinances App - First Screen created, Second Screen on track

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Button } from '../../components/Form/Button';
+import { Input } from '../../components/Form/Input';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+import { CategorySelect } from '../../components/Form/CategorySelect';
+
+
+
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+  TransactionsTypes
+} from './styles';
+
+export function Register() {
+  const [transactionType, setTransactionType] = useState('')
+
+  function handleTransactionsTypeSelect(type: 'up' | 'down') {
+    setTransactionType(type);
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+
+      <Form>
+        <Fields>
+          <Input
+            placeholder="Nome"
+          />
+          <Input
+            placeholder="Valor"
+          />
+
+          <TransactionsTypes>
+            <TransactionTypeButton
+              type="up"
+              title="Entrada"
+              onPress={() => handleTransactionsTypeSelect('up')}
+              isActive={transactionType === 'up'}
+            />
+            <TransactionTypeButton
+              type="down"
+              title="Saída"
+              onPress={() => handleTransactionsTypeSelect('down')}
+              isActive={transactionType === 'down'}
+            />
+          </TransactionsTypes>
+
+          <CategorySelect title="Categoria" />
+        </Fields>
+
+        <Button title="Enviar" />
+      </Form>
+
+    </Container>
+  );
+}
